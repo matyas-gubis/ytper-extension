e70f6c3b8b02ac72cc6838e956792047e03ab3ec
Mátyás János Gubis <gubis@contens.de>
2024-08-20 10:36:51 +0200

    change keyboard to checkbox, handle changes with chrome storage

--- scripts/content.js
+++ scripts/content.js
@@ -1,54 +1,65 @@
 // document.body.style.backgroundColor = 'red'
-let keyboard = document.getElementById('Y-keyboard');
-let originalKeymap = 'qwertyuiopasdfghjklzxcvbnm';
-let mappedKeymap = '';
+let keyboard = document.getElementById("Y-keyboard");
+let originalKeymap = "qwertyuiopasdfghjklzxcvbnm";
+let mappedKeymap = "";
 let keymapIndex = 0;
 let stage = 1;
-let fruit = document.getElementById('fruit').textContent;
+let fruit = document.getElementById("fruit").textContent;
 let shouldSolve = false;
 let BACKSPACE_DELAY = 50;
 let MIN_SEND_DELAY = 100;
 let MAX_SEND_DELAY = 600;
 
+chrome.storage.sync.get(['keyboardVisible']).then(({keyboardVisible})=>{
+  if (!keyboard) {
+    keyboard = createKeyboard();
+  }
+  keyboard.hidden = !keyboardVisible;
+})
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log(request, sender);
-  if (request.action === 'switchKeyboard') {
-    handleKeyboardVisibilitySwitch();
-    sendResponse(keyboard.hidden);
-  }
-  if (request.action === 'mapKeys') {
+  if (request.action === "mapKeys") {
     mapKeys();
   }
-  if (request.action === 'solve') {
+  if (request.action === "solve") {
     handleSolve();
   }
 });
 
-if (localStorage.getItem('keyboardVisible') === 'true') {
-  keyboard ? (keyboard.hidden = true) : createKeyboard();
-}
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  console.log('change')
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    if (key === "keyboardVisible") {
+      keyboard.hidden = !newValue;
+    }
+    console.log(
+      `Storage key "${key}" in namespace "${namespace}" changed.`,
+      `Old value was "${oldValue}", new value is "${newValue}".`
+    );
+  }
+});
 
 const socket = io();
 
-socket.on('update_word', ({ current_word }) => {
+socket.on("update_word", ({ current_word }) => {
   handleReceivedWord(current_word);
 });
 
-socket.on('stage_success', ({ next_stage, next_fruit }) => {
+socket.on("stage_success", ({ next_stage, next_fruit }) => {
   stage = next_stage;
   fruit = next_fruit;
-  mappedKeymap = '';
+  mappedKeymap = "";
   keymapIndex = 0;
 });
 
 function handleReceivedWord(receivedWord) {
-  // console.log('update_word', receivedWord, keymapIndex, receivedWord.length, mappedKeymap, mappedKeymap.length);
   if (keymapIndex < 3 && receivedWord.length === 9) {
     mappedKeymap += receivedWord;
     console.log(`letters '${mappedKeymap}' mapped`);
     setTimeout(() => {
       keymapIndex++;
-      console.log('mapping keys', keymapIndex);
+      console.log("mapping keys", keymapIndex);
       mapKeys();
     }, (1 + receivedWord.length) * BACKSPACE_DELAY);
     deleteWord(receivedWord);
@@ -56,9 +67,10 @@ function handleReceivedWord(receivedWord) {
     deleteWord(receivedWord);
     mappedKeymap += receivedWord;
     keymapIndex++;
-    console.log('keyboard fully mapped');
-    originalKeymap.split('').forEach((letter, i) => {
-      document.getElementById(`Y-keycap-${letter}`).textContent = mappedKeymap.charAt(i);
+    console.log("keyboard fully mapped");
+    originalKeymap.split("").forEach((letter, i) => {
+      document.getElementById(`Y-keycap-${letter}`).textContent =
+        mappedKeymap.charAt(i);
     });
   } else if (
     keymapIndex >= 3 &&
@@ -67,18 +79,22 @@ function handleReceivedWord(receivedWord) {
     receivedWord.length !== 0 &&
     fruit.slice(0, receivedWord.length) !== receivedWord
   ) {
-    console.log('remove:', receivedWord);
+    console.log("remove:", receivedWord);
     setTimeout(() => {
-      sendLetter('backspace');
+      sendLetter("backspace");
     }, BACKSPACE_DELAY);
-  } else if (shouldSolve && receivedWord.length === 0 && mappedKeymap.length === originalKeymap.length) {
+  } else if (
+    shouldSolve &&
+    receivedWord.length === 0 &&
+    mappedKeymap.length === originalKeymap.length
+  ) {
     console.log(
-      'send solution:',
+      "send solution:",
       fruit,
       fruit
-        .split('')
+        .split("")
         .map((l) => mappedKeymap.charAt(originalKeymap.indexOf(l)))
-        .join('')
+        .join("")
     );
     sendSolution();
   }
@@ -86,9 +102,9 @@ function handleReceivedWord(receivedWord) {
 
 function sendSolution() {
   let previousDelay = getDelay();
-  fruit.split('').forEach((letter, i) => {
+  fruit.split("").forEach((letter, i) => {
     setTimeout(() => {
-      console.log('sending letter', letter);
+      console.log("sending letter", letter);
       sendLetter(originalKeymap.charAt(mappedKeymap.indexOf(letter)));
     }, previousDelay);
     previousDelay += getDelay();
@@ -99,7 +115,7 @@ function mapKeys() {
   let lastDelay = MIN_SEND_DELAY;
   originalKeymap
     .slice(keymapIndex * 9, keymapIndex * 9 + 9)
-    .split('')
+    .split("")
     .forEach((letter) => {
       setTimeout(() => {
         sendLetter(letter);
@@ -108,31 +124,21 @@ function mapKeys() {
     });
 }
 
-function handleKeyboardVisibilitySwitch() {
-  if (!keyboard) {
-    createKeyboard();
-    localStorage.setItem('keyboardVisible', 'true');
-  } else {
-    keyboard.hidden = !keyboard.hidden;
-    localStorage.setItem('keyboardVisible', !keyboard.hidden + '');
-  }
-}
-
 function createKeyboard() {
-  const kb = document.createElement('div');
-  kb.classList.add('Y-keyboard', 'Y-base');
-  kb.id = 'Y-keyboard';
+  const kb = document.createElement("div");
+  kb.classList.add("Y-keyboard", "Y-base");
+  kb.id = "Y-keyboard";
   document.body.appendChild(kb);
-  ['qwertyuiop', 'asdfghjkl', 'zxcvbnm'].forEach((row, rowindex) => {
-    const rowElement = document.createElement('div');
-    rowElement.classList.add('Y-keyboard-row', 'Y-base');
-    row.split('').forEach((letter) => {
-      const button = document.createElement('div');
-      button.classList.add('Y-keycap', 'Y-base');
-      const span1 = document.createElement('span');
-      const span2 = document.createElement('span');
+  ["qwertyuiop", "asdfghjkl", "zxcvbnm"].forEach((row, rowindex) => {
+    const rowElement = document.createElement("div");
+    rowElement.classList.add("Y-keyboard-row", "Y-base");
+    row.split("").forEach((letter) => {
+      const button = document.createElement("div");
+      button.classList.add("Y-keycap", "Y-base");
+      const span1 = document.createElement("span");
+      const span2 = document.createElement("span");
       span1.textContent = letter;
-      span1.classList.add('small');
+      span1.classList.add("small");
       span2.id = `Y-keycap-${letter}`;
       span2.textContent = letter;
       button.appendChild(span1);
@@ -141,11 +147,11 @@ function createKeyboard() {
     });
     kb.appendChild(rowElement);
   });
-  keyboard = document.getElementById('Y-keyboard');
+  return keyboard = document.getElementById("Y-keyboard");
 }
 
 function handleSolve() {
-  console.log('solve');
+  console.log("solve");
   shouldSolve = true;
   if (mappedKeymap.length === originalKeymap.length) {
     sendSolution();
@@ -155,20 +161,22 @@ function handleSolve() {
 }
 
 function getDelay() {
-  return Math.floor(Math.random() * MAX_SEND_DELAY - MIN_SEND_DELAY) + MIN_SEND_DELAY;
+  return (
+    Math.floor(Math.random() * MAX_SEND_DELAY - MIN_SEND_DELAY) + MIN_SEND_DELAY
+  );
 }
 
 function sendLetter(letter) {
-  socket.emit('character_input', {
+  socket.emit("character_input", {
     char: letter,
     stage: stage,
   });
 }
 
 function deleteWord(current_word) {
-  current_word.split('').forEach((_, i) => {
+  current_word.split("").forEach((_, i) => {
     setTimeout(() => {
-      sendLetter('backspace');
+      sendLetter("backspace");
     }, i * BACKSPACE_DELAY);
   });
 }
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,5 @@
-const container = document.getElementById('container')
-const wrongpage = document.getElementById('wrongpage')
+const container = document.getElementById("container");
+const wrongpage = document.getElementById("wrongpage");
 
 const showKeyboardCheckbox = document.getElementById("showkeyboard");
 const autoMapKeysCheckbox = document.getElementById("mapkeys");
@@ -12,23 +12,26 @@ solveBtn.addEventListener("click", handleSolveClicked);
 
 // mapKeysBtn.hidden = localStorage.getItem('keyboardVisible') ? true : false;
 
-chrome.tabs.query({active: true}).then(([tab])=> {
-  if(tab.url.includes('ytper.com/play')){
-    container.hidden = false
-    wrongpage.hidden = true
-  }else{
-    container.hidden = true
-    wrongpage.hidden = false
+chrome.tabs.query({ active: true }).then(([tab]) => {
+  if (tab.url.includes("ytper.com/play")) {
+    container.hidden = false;
+    wrongpage.hidden = true;
+  } else {
+    container.hidden = true;
+    wrongpage.hidden = false;
   }
-})
+});
 
 chrome.storage.sync.get(["keyboardVisible"]).then(({ keyboardVisible }) => {
   showKeyboardCheckbox.checked = keyboardVisible;
-  mapKeysBtn.disabled = !keyboardVisible;
+  mapKeysBtn.disabled = keyboardVisible;
 });
 
 async function handleShowKeyboardChanged(e) {
-  chrome.storage.sync.set({keyboardVisible: e.target.checked}).then()
+  chrome.storage.sync.set({ keyboardVisible: e.target.checked }).then(() => {
+    showKeyboardCheckbox.checked = e.target.checked;
+    mapKeysBtn.disabled = !e.target.checked;
+  });
 }
 
 async function handleMapkeysClicked(e) {
