322ff861fb0e96f945877428b988bb63f490284f
Mátyás János Gubis <gubis@contens.de>
2024-08-20 10:07:25 +0200

    add 'only works on ytper.com' error when not on the correct page; warning: breaks popup-content communication

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,31 +1,42 @@
-const showKeyboardBtn = document.getElementById('showkeyboard');
-const mapKeysBtn = document.getElementById('mapkeys');
-const solveBtn = document.getElementById('solve');
+const container = document.getElementById('container')
+const wrongpage = document.getElementById('wrongpage')
 
-showKeyboardBtn.addEventListener('click', handleShowKeyboardClicked);
-mapKeysBtn.addEventListener('click', handleMapkeysClicked);
-solveBtn.addEventListener('click', handleSolveClicked);
+const showKeyboardCheckbox = document.getElementById("showkeyboard");
+const autoMapKeysCheckbox = document.getElementById("mapkeys");
+const mapKeysBtn = document.getElementById("mapkeys");
+const solveBtn = document.getElementById("solve");
 
-mapKeysBtn.hidden = localStorage.getItem('keyboardVisible') ? true : false;
+showKeyboardCheckbox.addEventListener("click", handleShowKeyboardChanged);
+autoMapKeysCheckbox.addEventListener("click", handleMapkeysClicked);
+solveBtn.addEventListener("click", handleSolveClicked);
 
-async function handleShowKeyboardClicked(e) {
-  const [tab] = await chrome.tabs.query({ active: true });
-  const response = await chrome.tabs.sendMessage(tab.id, { action: 'switchKeyboard' });
-  if (response) {
-    showKeyboardBtn.textContent = 'Show keyboard';
-    mapKeysBtn.hidden = true;
-  } else {
-    showKeyboardBtn.textContent = 'Hide keyboard';
-    mapKeysBtn.hidden = false;
+// mapKeysBtn.hidden = localStorage.getItem('keyboardVisible') ? true : false;
+
+chrome.tabs.query({active: true}).then(([tab])=> {
+  if(tab.url.includes('ytper.com/play')){
+    container.hidden = false
+    wrongpage.hidden = true
+  }else{
+    container.hidden = true
+    wrongpage.hidden = false
   }
+})
+
+chrome.storage.sync.get(["keyboardVisible"]).then(({ keyboardVisible }) => {
+  showKeyboardCheckbox.checked = keyboardVisible;
+  mapKeysBtn.disabled = !keyboardVisible;
+});
+
+async function handleShowKeyboardChanged(e) {
+  chrome.storage.sync.set({keyboardVisible: e.target.checked}).then()
 }
 
 async function handleMapkeysClicked(e) {
   const [tab] = await chrome.tabs.query({ active: true });
-  chrome.tabs.sendMessage(tab.id, { action: 'mapKeys' });
+  chrome.tabs.sendMessage(tab.id, { action: "mapKeys" });
 }
 
 async function handleSolveClicked(e) {
   const [tab] = await chrome.tabs.query({ active: true });
-  chrome.tabs.sendMessage(tab.id, { action: 'solve' });
+  chrome.tabs.sendMessage(tab.id, { action: "solve" });
 }
