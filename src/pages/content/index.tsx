fe46b00b9750234b836684a32408a5f7b6d41c1d
matyasgubis <gubismatyas@gmail.com>
2024-08-22 20:05:12 +0200

    feat: init react and vite

---
+++ src/pages/content/index.tsx
@@ -0,0 +1,16 @@
+import { createRoot } from 'react-dom/client';
+import './style.css';
+const div = document.createElement('div');
+div.id = '__root';
+document.body.appendChild(div);
+
+const rootContainer = document.querySelector('#__root');
+if (!rootContainer) throw new Error("Can't find Content root element");
+const root = createRoot(rootContainer);
+root.render(<div>content script loaded</div>);
+
+try {
+  console.log('content script loaded');
+} catch (e) {
+  console.error(e);
+}
---
+++ src/pages/popup/Popup.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '@assets/img/logo.svg';
+
+export default function Popup(): JSX.Element {
+  return (
+    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
+      <header className="flex flex-col items-center justify-center text-white">
+        <p>Hello</p>
+      </header>
+    </div>
+  );
+}
