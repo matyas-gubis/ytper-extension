4a50fa109971dc66bad5e89b3451828332420d84
matyasgubis <gubismatyas@gmail.com>
2024-08-24 00:31:53 +0200

    initialize wxt project with react, tailwind, content and popup

--- old/src/popup/Popup.tsx
+++ old/src/popup/Popup.tsx
@@ -8,19 +8,11 @@ function Popup() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>This sucks</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
-          Edit <code>src/Popup.tsx</code> and save to test HMR
+          Edit <code>src/Popup.tsx</code> and save to test HMR 2
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
